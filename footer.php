6a261cb7816c7823bbdf0c91c039c5886195516f
Luke Eller <luke.eller@bigcommerce.com>
2013-02-17 13:22:45 +1100

    iPad styles for front page.

--- footer.php
+++ footer.php
@@ -2,13 +2,12 @@
             </div><!-- .row -->
             <footer class="row" role="contentinfo">
                 <span id="support" class="span4">Proudly supported by <a href="http://syd.swingpatrol.com/">Swing Patrol</a></span>
-                <div class="offset3 span5">
-                    <?php wp_nav_menu( array( 'theme_location' => 'site-info', 'menu_class' => 'menu menu-secondary list-horizontal' ) ); ?>
+                <div class="span8">
+                    <?php wp_nav_menu( array( 'theme_location' => 'site-info', 'menu_class' => 'menu menu-secondary' ) ); ?>
                     <span id="copyright" class="pull-right">&copy; 2013 Harbour City Hoppers</span>
                 </div>
             </footer>
         </div> <!-- .container -->
-        <script src="http://code.jquery.com/jquery-latest.js"></script>
         <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -9,24 +9,25 @@
         <!--[if lt IE 9]>
         <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
         <![endif]-->
+        <?php wp_enqueue_script("jquery"); ?>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
         <div class="container">
             <div class="row">
                 <header class="header">
-                    <div class="span5">
+                    <div class="span4 header-image-container">
                         <?php $header_image = get_header_image();
                         if ( ! empty( $header_image ) ) : ?>
                             <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
                         <?php endif; ?>
                     </div>
-                    <div class="span7 header-links">
+                    <div class="span8 header-links">
                         <?php get_template_part( 'social-media' ); ?>
                         
                         <?php wp_nav_menu( array( 
                             'theme_location' => 'primary',
-                            'menu_class' => 'menu menu-primary list-horizontal',
+                            'menu_class' => 'menu menu-primary',
                             'items_wrap' => '<button type="button" class="visible-phone menu-toggle" data-toggle="dropdown">Menu</button><ul id="%1$s" class="%2$s">%3$s</ul>'
                         ) ); ?>
                     </div>
