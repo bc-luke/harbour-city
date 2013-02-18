687b22428c1bed5e430aff0c2ea1b14c05c3b669
Luke Eller <luke.eller@bigcommerce.com>
2013-02-18 22:36:24 +1100

    Added index content.

--- content.php
+++ content.php
@@ -1,27 +1,7 @@
-<?php
-/**
- * Template Name: Front Page Template
- *
- * Description: A page template that provides a key component of WordPress as a CMS
- * by meeting the need for a carefully crafted introductory page. The front page template
- * in Twenty Twelve consists of a page content area for adding text, images, video --
- * anything you'd like -- followed by front-page-only widgets in one or two columns.
- *
- * @package WordPress
- * @subpackage Harbour_City
- * @since Harbour City 1.0
- */
-
-get_header(); ?>
-
-<div id="front-page" class="main" role="main">
-
-    <?php while ( have_posts() ) : the_post(); ?>
-
             <div class="row">
                 <div class="offset7 span5">
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <header class="entry-header hide">
+                        <header class="entry-header">
                             <h1 class="entry-title"><?php the_title(); ?></h1>
                         </header>
 
@@ -35,11 +15,3 @@ get_header(); ?>
                     </article><!-- #post -->
                 </div>
             </div>
-
-    <?php endwhile; // end of the loop. ?>
-
-</div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
-                
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php get_header(); ?>
+
+<div id="primary" class="main" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'content', get_post_format() ); ?>
+
+    <?php endwhile; // end of the loop. ?>
+
+</div>
+
 <?php get_footer(); ?>
